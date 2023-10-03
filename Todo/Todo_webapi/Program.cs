1874d414e1eacdd99ba126209be7ffd6bcd9e91d
Alexander Kim <nosensus@gmail.com>
2023-10-03 22:43:59 +0500

    feat: editorconfig for .cs files

--- Todo/Todo_webapi/Program.cs
+++ Todo/Todo_webapi/Program.cs
@@ -10,10 +10,9 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -23,4 +22,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
